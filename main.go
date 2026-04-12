83b4c66aea319e509c3cd5eb93954d78a4c1eba3
Pipastalk <pipalabs@luckyfixes.co.uk>
2026-04-12 19:09:57 +0100

    Adding Chrip validations

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sync/atomic"
@@ -22,11 +23,13 @@ func main() {
 	serveMux.Handle("/app/", apiMetrics.middlewareMetricsInc(fileServer))
 	serveMux.HandleFunc("GET /admin/metrics", middlewareAdminCalls(http.HandlerFunc(apiMetrics.fileServerHitsHandler)))
 	serveMux.HandleFunc("POST /admin/reset", middlewareAdminCalls(http.HandlerFunc(apiMetrics.resetHandler)))
+	serveMux.HandleFunc("POST /api/validate_chirp", middlewareAPICalls(http.HandlerFunc(apiMetrics.validateChirpHandler)))
 	httpServer.ListenAndServe()
 }
 func fileServerHandler() http.Handler {
 	return http.Handler(http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 }
+
 func readyCheck(w http.ResponseWriter, r *http.Request) {
 	headers := w.Header()
 	body := "OK"
@@ -78,4 +81,57 @@ func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.Write([]byte("Hits reset\n"))
+}
+
+func (cfg *apiConfig) validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	type chirp struct {
+		Message string `json:"body"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	var c chirp
+	if err := decoder.Decode(&c); err != nil {
+		respondWithError(w, http.StatusNoContent, "Invalid JSON")
+		return
+	}
+	if len(c.Message) == 0 {
+		respondWithError(w, http.StatusBadRequest, "Message cannot be empty")
+		return
+	}
+	charLimit := 140
+	if len(c.Message) > charLimit {
+		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+	payload := struct {
+		Valid bool `json:"valid"`
+	}{
+		Valid: true,
+	}
+	respondWithJSON(w, http.StatusOK, payload)
+}
+
+func respondWithError(w http.ResponseWriter, statusCode int, message string) {
+	w.WriteHeader(statusCode)
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+	e := errorResponse{Error: message}
+	jsonData, err := json.Marshal(e)
+	if err != nil {
+		w.Write([]byte(`{"error": "Failed to encode error response"}`))
+		return
+	}
+	w.Write(jsonData)
+}
+
+func respondWithJSON(w http.ResponseWriter, statusCode int, payload interface{}) {
+	w.WriteHeader(statusCode)
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	jsonData, err := json.Marshal(payload)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to encode response")
+		return
+	}
+	w.Write(jsonData)
 }
